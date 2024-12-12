8f496b5e46866f3c4aae16998d416c9e9996e142
Ramya <ramyareddym023@gmail.com>
2024-12-12 12:38:30 +0530

    Add files via upload

---
+++ Array_List.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Array_List{
+    public static void main(String args[])
+    {
+        ArrayList <Integer> a=new ArrayList <> ();
+        a.add(345);
+        a.add(145);
+        a.add(1002);
+        System.out.println(a);
+
+    }
+}
