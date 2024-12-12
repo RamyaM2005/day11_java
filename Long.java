597500df510b0abe0e54af510eb26c840697746a
Ramya <ramyareddym023@gmail.com>
2024-12-12 13:40:40 +0530

    Add files via upload

---
+++ Long.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Long{
+    public static void main(String args[])
+    {
+        long ans=90;
+        Scanner sc=new Scanner(System.in);
+        //Accepting Long
+        long s=sc.nextLong();
+        System.out.println(ans);
+        System.out.println(s);
+        sc.close();
+    }
+}
